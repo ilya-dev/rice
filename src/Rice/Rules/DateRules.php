0b5dc0bc22ad3bd662d0fef654d95ae5ea4aadfa
Ilya S <public.ilya.s@gmail.com>
2014-04-23 20:33:13 +0700

    add ::between

--- src/Rice/Rules/DateRules.php
+++ src/Rice/Rules/DateRules.php
@@ -2,7 +2,20 @@
 
 class DateRules {
 
+    /**
+     * Check whether the given value is in the range (inclusively).
+     *
+     * @param \DateTime $value
+     * @param \DateTime $min
+     * @param \DateTime $max
+     * @return boolean
+     */
+    public function between(\DateTime $value, \DateTime $min, \DateTime $max)
+    {
+        list($value, $min, $max) = [$value->getTimestamp(), $min->getTimestamp(), $max->getTimestamp()];
 
+        return ($min <= $value) and ($value <= $max);
+    }
 
 }
 
--- tests/Spec/Rice/Rules/DateRulesSpec.php
+++ tests/Spec/Rice/Rules/DateRulesSpec.php
@@ -10,5 +10,22 @@ class DateRulesSpec extends ObjectBehavior {
         $this->shouldHaveType('Rice\Rules\DateRules');
     }
 
+    function it_checks_whether_a_value_fits_the_range()
+    {
+        $format = 'Y-m-d H:i:s';
+
+        $value = \DateTime::createFromFormat($format, '2014-04-23 20:27:30');
+
+        $min = \DateTime::createFromFormat($format, '2014-04-23 18:00:00');
+
+        $max = \DateTime::createFromFormat($format, '2014-04-23 20:00:00');
+
+        $this->between($value, $min, $max)->shouldReturn(false);
+
+        $max = \DateTime::createFromFormat($format, '2014-04-23 21:00:00');
+
+        $this->between($value, $min, $max)->shouldReturn(true);
+    }
+
 }
 
