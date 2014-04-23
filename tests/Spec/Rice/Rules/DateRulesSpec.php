eb8e85f0661358ef5461502486d6421f4b56536e
Ilya S <public.ilya.s@gmail.com>
2014-04-23 20:35:01 +0700

    tests cleanup

--- tests/Spec/Rice/Rules/DateRulesSpec.php
+++ tests/Spec/Rice/Rules/DateRulesSpec.php
@@ -12,17 +12,14 @@ class DateRulesSpec extends ObjectBehavior {
 
     function it_checks_whether_a_value_fits_the_range()
     {
-        $format = 'Y-m-d H:i:s';
-
-        $value = \DateTime::createFromFormat($format, '2014-04-23 20:27:30');
-
-        $min = \DateTime::createFromFormat($format, '2014-04-23 18:00:00');
-
-        $max = \DateTime::createFromFormat($format, '2014-04-23 20:00:00');
+        $format = 'H:i:s';
+        $value  = \DateTime::createFromFormat($format, '20:27:30');
+        $min    = \DateTime::createFromFormat($format, '18:00:00');
+        $max    = \DateTime::createFromFormat($format, '20:00:00');
 
         $this->between($value, $min, $max)->shouldReturn(false);
 
-        $max = \DateTime::createFromFormat($format, '2014-04-23 21:00:00');
+        $max = \DateTime::createFromFormat($format, '21:00:00');
 
         $this->between($value, $min, $max)->shouldReturn(true);
     }
